95ce34d3acabef67117e5a6547e13e62c72df71a
Patrick Santos <Patricksantosribeiro2017@gmail.com>
2024-01-02 21:14:38 -0300

    Initial commit

---
+++ src/components/ConteudoPrincipal.vue
@@ -0,0 +1,101 @@
+<script lang="ts">
+import MostrarReceitas from './MostrarReceitas.vue';
+import SelecionarIngredientes from './SelecionarIngredientes.vue';
+import SuaLista from './SuaLista.vue';
+import Tag from './Tag.vue';
+
+type Pagina = 'SelecionarIngredientes' | 'MostrarReceitas';
+
+export default {
+  data() {
+    return {
+      ingredientes: [] as string[],
+      conteudo: 'SelecionarIngredientes' as Pagina
+    };
+  },
+  components: { SelecionarIngredientes, Tag, SuaLista, MostrarReceitas },
+  methods: {
+    adicionarIngrediente(ingrediente: string) {
+      this.ingredientes.push(ingrediente)
+    },
+    removerIngrediente(ingrediente: string) {
+      this.ingredientes = this.ingredientes.filter(iLista => ingrediente !== iLista);
+    },
+    navegar(pagina: Pagina) {
+      this.conteudo = pagina;
+    }
+  }
+}
+</script>
+
+<template>
+  <main class="conteudo-principal">
+    <SuaLista :ingredientes="ingredientes" />
+
+    <KeepAlive include="SelecionarIngredientes">
+      <SelecionarIngredientes v-if="conteudo === 'SelecionarIngredientes'"
+        @adicionar-ingrediente="adicionarIngrediente"
+        @remover-ingrediente="removerIngrediente"
+        @buscar-receitas="navegar('MostrarReceitas')"
+      />
+  
+      <MostrarReceitas v-else-if="conteudo === 'MostrarReceitas'"
+        :ingredientes="ingredientes"
+        @editar-receitas="navegar('SelecionarIngredientes')"
+      />
+    </KeepAlive>
+  </main>
+</template>
+
+<style scoped>
+.conteudo-principal {
+  padding: 6.5rem 7.5rem;
+  border-radius: 3.75rem 3.75rem 0rem 0rem;
+  background: var(--creme, #FFFAF3);
+  color: var(--cinza, #444);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5rem;
+}
+
+.sua-lista-texto {
+  color: var(--coral, #F0633C);
+  display: block;
+  text-align: center;
+  margin-bottom: 1.5rem;
+}
+
+.ingredientes-sua-lista {
+  display: flex;
+  justify-content: center;
+  gap: 1rem 1.5rem;
+  flex-wrap: wrap;
+}
+
+.lista-vazia {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 0.25rem;
+
+  color: var(--coral, #F0633C);
+  text-align: center;
+}
+
+@media only screen and (max-width: 1300px) {
+  .conteudo-principal {
+    padding: 5rem 3.75rem;
+    gap: 3.5rem;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .conteudo-principal {
+    padding: 4rem 1.5rem;
+    gap: 4rem;
+  }
+}
+</style>
---
+++ src/http/index.ts
@@ -0,0 +1,15 @@
+import type ICategoria from "@/interfaces/ICategoria";
+import type IReceita from "@/interfaces/IReceita";
+
+async function obterDadosURL<T>(url: string) {
+  const resposta = await fetch(url);
+  return resposta.json() as T;
+}
+
+export async function obterCategorias() {
+  return obterDadosURL<ICategoria[]>('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/86802bed06855cdccc9247dd1fa175c6ccea7dda/categorias.json');
+}
+
+export async function obterReceitas() {
+  return obterDadosURL<IReceita[]>('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/07e853b7d0626db51ce2e84bb2f15ca450b7bd7f/receitas.json');
+}
